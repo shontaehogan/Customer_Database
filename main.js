52a0aed515229765e846e0a058bf298abb86f2d2
Shontae Hogan <shontaehogan@gmail.com>
2017-07-25 01:21:33 -0400

    made changes to code to reflect the usage of string literals

--- main.js
+++ main.js
@@ -1,57 +1,33 @@
-(function() {
-  'use strict';
+let allCustomers = `
+<header>
+  <h1>Internal Company Directory</h1>
+</header>
+<span class="break">`
 
-  const allCustomers = document.querySelector('.customers');
 
-  fetch('https://randomuser.me/api/?format=json&results=12')
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(data) {
-      customerData(data.results);
-    });
+customers.results.map(function(item){
+	allCustomers += `
+		<div class="all">
+			<div class="img">
+				<img src="${item.picture.large}">
+				<h4>${item.name.first} ${item.name.last}</h4>
+			</div>
+			<div class="personEmail">
+				<span class="email">${item.email}</span>
+			</div>
+			<div class="personAddPhone">
+				<span class="street">${item.location.street}</span>
+        <span>
 
-  function customerData(arr) {
-    for (let i in arr) {
-      const person = arr[i];
-      let personName = person.name.first + " " + person.name.last;
-      let personEmail = person.email;
-      let personAddr1 = person.location.street;
-      let personAddr2 = person.location.city + ", " + person.location.state + ", " + person.location.postcode;
-      let personPhone = person.phone;
-      let personSSN = person.value;
-      let box = document.createElement('div');
-      let img = document.createElement('img');
-      img.setAttribute('src', person.picture.large);
-      box.appendChild(img);
+        <span class="location">${item.location.city}, ${item.location.state}, ${item.location.postcode}</span>
 
-      let name = document.createElement('p');
-      name.textContent = personName.toUpperCase();
-      name.classList.add('name');
-      box.appendChild(name);
+				<span class="phone">${item.cell}</span>
+			</div>
+			<div class="personSSN">
+				<span class="ssn"> ${item.id.value}</span>
+			</div>
+		</div>
+	`
+});
 
-      let email = document.createElement('p');
-      email.textContent = personEmail.toUpperCase();
-      email.classList.add('email');
-      box.appendChild(email);
-
-      let address = document.createElement('p');
-      address.innerHTML = personAddr1 + "<br>" + personAddr2;
-      address.classList.add('address');
-      box.appendChild(address);
-
-      let phone = document.createElement('p');
-      phone.textContent = personPhone;
-      phone.classList.add('phone');
-      box.appendChild(phone);
-      allCustomers.appendChild(box);
-
-      let ssn = document.createElement('ssn');
-      ssn.textContent = personSSN;
-      ssn.classList.add('ssn');
-      box.appendChild(ssn);
-      allCustomers.appendChild(box);
-
-    }
-  }
-})();
+document.querySelector("#customers").innerHTML = allCustomers
